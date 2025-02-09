f0e0de6d04fe4c8318366647515272667c1be74f
Rakesh Ayyaswami <rakeshta@gmail.com>
2025-02-10 02:16:05 +1100

    icon for ammo category

--- prototypes/categories/ammo.lua
+++ prototypes/categories/ammo.lua
@@ -5,8 +5,12 @@
 --  Created by Rakesh Ayyaswami on 16 Jan 2023.
 --
 
+local graphics_path = mod_root_path .. "graphics/ammo/"
+
 data.raw["ammo-category"]["anti-material-rifle-ammo"] = {
   bonus_gui_order = "t",
   type = "ammo-category",
   name = "anti-material-rifle-ammo",
+  icon = graphics_path .. "anti-material-rifle-ammo.png",
+  subgroup = "ammo-category"
 }
